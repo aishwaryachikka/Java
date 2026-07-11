65fcd3fe13fc90daf905c74b386e27eb2d5a8b36
Aishwarya Chikka <aishwaryachikka@gmail.com>
2026-07-11 15:48:27 +0530

    Add files via upload

---
+++ Code space/tue.java
@@ -0,0 +1,15 @@
+public class tue{
+	public static void main(String args[]){
+		int intval=10;
+		float floatval=10;
+		double doubleval=10;
+		float f=6/5;
+		char gender ='f';
+	    System.out.println("int val"+intval);
+		System.out.println("float val"+floatval);
+		System.out.println("doubleval"+doubleval);
+		System.out.println(f);
+		System.out.println(gender);
+		
+	}
+}
