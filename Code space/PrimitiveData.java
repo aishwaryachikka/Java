578ba3d54f88e4f435c429539c8e50764aadf269
Aishwarya Chikka <aishwaryachikka@gmail.com>
2026-08-25 16:20:11 +0530

    Add files via upload

---
+++ Code space/PrimitiveData.java
@@ -0,0 +1,20 @@
+public class PrimitiveData{
+	public static void main(String args[]){
+		byte b=100;
+		short s= 233;
+		int n= 23;
+		long l= 3467889L;
+		float f= 34.567f;
+		double d=207.5443322;
+		char c='a';
+		boolean bo = true;
+		System.out.println("Byte: "+b);
+		System.out.println("Short: "+s);
+		System.out.println("Integer: "+n);
+		System.out.println("long: "+l);
+        System.out.println("Float: "+f);
+        System.out.println("Double: "+d);
+        System.out.println("Character: "+c);
+		System.out.println("Boolean: "+bo);
+	}
+}
---
+++ Code space/controlstatements.java
@@ -0,0 +1,15 @@
+public class controlstatements{
+	public static void main(String args[]){
+		int marks=90;
+		if(marks>=90 && marks<=100){
+			System.out.println("gradeA");
+		}
+		else if(marks>=80 && marks <=90){
+			System.out.println("gradeB");
+		}
+		else{
+			System.out.println("fail");
+		}
+		
+	}
+}
